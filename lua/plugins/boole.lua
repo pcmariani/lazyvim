cc3186ecbcc482370cda56cb92619c9e84a1f939
Pete Mariani <pcmariani@gmail.com>
2025-08-07 14:49:13 -0400

    initial

---
+++ lua/plugins/boole.lua
@@ -0,0 +1,24 @@
+return {
+  "nat-418/boole.nvim",
+  enabled = false,
+  config = function()
+    require("boole").setup({
+      mappings = {
+        increment = "gb",
+        decrement = "gB",
+      },
+      -- User defined loops
+      additions = {
+        { "Foo", "Bar" },
+        { "tic", "tac", "toe" },
+        { "ascending", "descending" },
+      },
+      allow_caps_additions = {
+        { "enable", "disable" },
+        -- enable → disable
+        -- Enable → Disable
+        -- ENABLE → DISABLE
+      },
+    })
+  end,
+}
